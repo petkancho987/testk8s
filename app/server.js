465e8bd11c086185951acc5e2319242b5efc20e6
petkancho987 <petkan6993@gmail.com>
2026-09-04 09:07:35 +0300

    Change app and status values to 'test'

--- app/server.js
+++ app/server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.json({
-    app: 'lele-male-laptop',
-    status: 'lele-male-laptop',
+    app: 'test',
+    status: 'test,
     timestamp: new Date()
   });
 });

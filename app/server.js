31bf0249cb734bfcec89a8959a61a00c25f3857c
petkancho987 <petkan6993@gmail.com>
2026-09-04 09:45:02 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.json({
-    app: 'tester',
-    status: 'tester,
+    app: 'alabala',
+    status: 'alabala',
     timestamp: new Date()
   });
 });
